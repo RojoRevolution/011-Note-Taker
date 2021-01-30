d4fac8027bd31eae39c7bab015de468cbb51cab2
David Rojo <rojo@rojorevolution.com>
2021-01-29 22:20:13 -0600

    current issue seems to be I am pushing an array, when I need to be adding the note data into an object

---
+++ data/noteData.js
@@ -0,0 +1,5 @@
+const noteObject = {
+
+};
+
+module.exports = noteObject;
--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -2,6 +2,8 @@
 
 const dbJSON = require('../db/db.json');
 const fs = require('fs');
+const noteData = require('../data/noteData');
+
 
 module.exports = (app) => {
     // Get dbJSON from path /api/notes
@@ -9,9 +11,13 @@ module.exports = (app) => {
 
     app.post('/api/notes', (req, res) => {
         console.log(req)
-        const newNote = JSON.stringify(req.body);
+        // const newNote = JSON.stringify(req.body);
+        const newNote = req.body;
+        noteData.push(newNote)
+        console.log(noteData)
 
-        fs.writeFile('db/db.json', newNote, (err) => {
+
+        fs.appendFile('db/db-test.json', JSON.stringify(noteData), (err) => {
             if (err) throw err;
             console.log('Body added to DB')
         })
